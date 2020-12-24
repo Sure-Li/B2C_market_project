7992fc1bcd2a6f944e8b5e6b6535baa12c6478f2
lishuo <1344793627@qq.com>
2020-12-24 11:58:14 +0800

    lombok-1.setter getter引入使用

--- b2cmarket/src/main/java/com/sureli/b2cmarket/order/pojo/Order.java
+++ b2cmarket/src/main/java/com/sureli/b2cmarket/order/pojo/Order.java
@@ -8,10 +8,15 @@ package com.sureli.b2cmarket.order.pojo;
 
 import com.sureli.b2cmarket.base.pojo.BaseClass;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @ClassName:Order
  * @Description:(订单类)
  */
+@Setter
+@Getter
 public class Order extends BaseClass {
 
 	private static final long serialVersionUID = 1L;
@@ -27,54 +32,6 @@ public class Order extends BaseClass {
 	private Integer orderState;
 	// 支付方式 1：到付 2：微信 3：支付宝 4：信用卡
 	private Integer orderPayMethod;
-
-	public String getOrderCode() {
-		return orderCode;
-	}
-
-	public void setOrderCode(String orderCode) {
-		this.orderCode = orderCode;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public Double getPriceSum() {
-		return priceSum;
-	}
-
-	public void setPriceSum(Double priceSum) {
-		this.priceSum = priceSum;
-	}
-
-	public String getAddressId() {
-		return addressId;
-	}
-
-	public void setAddressId(String addressId) {
-		this.addressId = addressId;
-	}
-
-	public Integer getOrderState() {
-		return orderState;
-	}
-
-	public void setOrderState(Integer orderState) {
-		this.orderState = orderState;
-	}
-
-	public Integer getOrderPayMethod() {
-		return orderPayMethod;
-	}
-
-	public void setOrderPayMethod(Integer orderPayMethod) {
-		this.orderPayMethod = orderPayMethod;
-	}
 
 	public Order() {
 		super();
